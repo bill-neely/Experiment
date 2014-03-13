df62fa0a31c3e9a195542893fe19196a7bb9bd7a
bill-neely <bneely@labattfood.com>
2014-03-13 17:57:45 -0500

    Starting

---
+++ ContactManager/ContactManager/Controllers/ContactController.vb
@@ -0,0 +1,81 @@
+﻿Namespace ContactManager
+    Public Class ContactController
+        Inherits System.Web.Mvc.Controller
+
+        '
+        ' GET: /Contact
+
+        Function Index() As ActionResult
+            Return View()
+        End Function
+
+        '
+        ' GET: /Contact/Details/5
+
+        Function Details(ByVal id As Integer) As ActionResult
+            Return View()
+        End Function
+
+        '
+        ' GET: /Contact/Create
+
+        Function Create() As ActionResult
+            Return View()
+        End Function
+
+        '
+        ' POST: /Contact/Create
+
+        <HttpPost> _
+        Function Create(ByVal collection As FormCollection) As ActionResult
+            Try
+                ' TODO: Add insert logic here
+                Return RedirectToAction("Index")
+            Catch
+                Return View()
+            End Try
+        End Function
+        
+        '
+        ' GET: /Contact/Edit/5
+
+        Function Edit(ByVal id As Integer) As ActionResult
+            Return View()
+        End Function
+
+        '
+        ' POST: /Contact/Edit/5
+
+        <HttpPost> _
+        Function Edit(ByVal id As Integer, ByVal collection As FormCollection) As ActionResult
+            Try
+                ' TODO: Add update logic here
+
+                Return RedirectToAction("Index")
+            Catch
+                Return View()
+            End Try
+        End Function
+
+        '
+        ' GET: /Contact/Delete/5
+
+        Function Delete(ByVal id As Integer) As ActionResult
+            Return View()
+        End Function
+
+        '
+        ' POST: /Contact/Delete/5
+
+        <HttpPost> _
+        Function Delete(ByVal id As Integer, ByVal collection As FormCollection) As ActionResult
+            Try
+                ' TODO: Add delete logic here
+
+                Return RedirectToAction("Index")
+            Catch
+                Return View()
+            End Try
+        End Function     
+    End Class
+End Namespace
---
+++ ContactManager/ContactManager/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.vb" Inherits="ContactManager.MvcApplication" Language="VB" %>
