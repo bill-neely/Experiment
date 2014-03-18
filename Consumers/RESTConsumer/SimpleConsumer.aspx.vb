01fc9e94408162a45d0aa53f0d24e5e118ab056d
bill-neely <bneely@labattfood.com>
2014-03-18 15:22:35 -0500

    proxy update

--- Consumers/RESTConsumer/SimpleConsumer.aspx.vb
+++ Consumers/RESTConsumer/SimpleConsumer.aspx.vb
@@ -18,10 +18,17 @@ Public Class SimpleConsumer
         Dim req As WebRequest
         Dim resp As WebResponse
 
-        'req = WebRequest.Create("http://default-environment-zg2axpn3f3.elasticbeanstalk.com/RestService/xml/" & txtParm.Text)
-        req = WebRequest.Create("http://localhost:8181/RestService/" & uri & "/" & txtParm.Text)
+
+
+        req = WebRequest.Create("http://default-environment-zg2axpn3f3.elasticbeanstalk.com/RestService/" & uri & "/" & txtParm.Text)
+        'req = WebRequest.Create("http://localhost:8181/RestService/" & uri & "/" & txtParm.Text)
+        req.Proxy = New System.Net.WebProxy("72.37.249.115", 8080)
         resp = req.GetResponse()
-        Dim respStream As System.IO.StreamReader = New System.IO.StreamReader(resp.GetResponseStream(), True)
+        'Dim respStream As System.IO.StreamReader = New System.IO.StreamReader(resp.GetResponseStream(), True)
+        Dim enc As Encoding = System.Text.Encoding.GetEncoding(1252)
+        Dim respStream As System.IO.StreamReader = New System.IO.StreamReader(resp.GetResponseStream(), enc)
+
+
         Return Server.HtmlEncode(respStream.ReadToEnd())
         
     End Function
